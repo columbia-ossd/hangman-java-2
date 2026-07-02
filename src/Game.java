ed9f1ee42acab9d2033a837290fb440100f53279
Byeolah-Kwon <byeolahk@gmail.com>
2026-07-02 11:43:58 -0400

    create game class skeleton

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
-    
+    private static final int MAX_ERRORS = 6;
+
+    private final Dictionary dictionary;
+    private String wordToFind;
+    private char[] wordFound;
+    private int numberOfErrors;
+    private ArrayList<String> guessedLetters;
+
+    public Game() {
+        dictionary = new Dictionary();
+        guessedLetters = new ArrayList<>();
+    }
+
+    public void newGame() {
+        numberOfErrors = 0;
+        guessedLetters.clear();
+
+        wordToFind = dictionary.getRandomWord();
+        wordFound = new char[wordToFind.length()];
+
+        wordFound[0] = wordToFind.charAt(0);
+
+        for (int i = 1; i < wordFound.length; i++) {
+            wordFound[i] = '_';
+        }
+
+        System.out.println("\nHint: the " + dictionary.getCategory()
+                + " starts with " + wordToFind.charAt(0));
+    }
 }
